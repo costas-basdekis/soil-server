33ed7d51e0ca5a97af47b1cfcc27d07bdc59e348
Costas Basdekis <costas.basdekis@gmail.com>
2020-08-21 09:31:21 +0100

    Refactor `bluetoothctl` a bit

--- bluetoothctl.py
+++ bluetoothctl.py
@@ -20,7 +20,7 @@
 
 # You should have received a copy of the GNU General Public License
 # along with ReachView.  If not, see <http://www.gnu.org/licenses/>.
-
+import re
 import time
 import pexpect
 import subprocess
@@ -40,81 +40,63 @@ class Bluetoothctl:
 
     def get_output(self, command, pause=0):
         """Run a command in bluetoothctl prompt, return output as a list of lines."""
-        self.child.send(command + "\n")
+        self.child.send("{}\n".format(command))
         time.sleep(pause)
         start_failed = self.child.expect(["bluetooth", pexpect.EOF])
 
         if start_failed:
-            raise BluetoothctlError("Bluetoothctl failed after running " + command.decode())
+            print("Error while running {}".format(command))
+            return None
 
         return self.child.before.decode().split("\r\n")
 
     def start_scan(self):
         """Start bluetooth scanning process."""
-        try:
-            return self.get_output("scan on")
-        except BluetoothctlError as e:
-            print(e)
-            return None
+        self.get_output("scan on")
 
     def make_discoverable(self):
         """Make device discoverable."""
-        try:
-            return self.get_output("discoverable on")
-        except BluetoothctlError as e:
-            print(e)
-            return None
+        return self.get_output("discoverable on")
+
+    def parse_device_infos(self, lines):
+        if not lines:
+            return []
+
+        devices = list(filter(None, (
+            self.parse_device_info(line)
+            for line in lines
+        )))
+
+        return devices
 
     def parse_device_info(self, info_string):
         """Parse a string corresponding to a device."""
-        device = {}
         block_list = ["[\x1b[0;", "removed"]
         string_valid = not any(keyword in info_string for keyword in block_list)
 
-        if string_valid:
-            try:
-                device_position = info_string.index("Device")
-            except ValueError:
-                pass
-            else:
-                if device_position > -1:
-                    attribute_list = info_string[device_position:].split(" ", 2)
-                    device = {
-                        "mac_address": attribute_list[1],
-                        "name": attribute_list[2],
-                    }
+        if not string_valid:
+            return None
+        if "Device" not in info_string:
+            return None
+        _, mac_address, name = info_string\
+            .split("Device", 1)[-1]\
+            .split(" ", 2)
+        device = {
+            "mac_address": mac_address,
+            "name": name,
+        }
 
         return device
 
     def get_available_devices(self):
         """Return a list of tuples of paired and discoverable devices."""
-        try:
-            out = self.get_output("devices")
-        except BluetoothctlError as e:
-            print(e)
-            return None
-
-        available_devices = list(filter(None, (
-            self.parse_device_info(line)
-            for line in out
-        )))
-
-        return available_devices
+        lines = self.get_output("devices")
+        return self.parse_device_infos(lines)
 
     def get_paired_devices(self):
         """Return a list of tuples of paired devices."""
-        try:
-            out = self.get_output("paired-devices")
-        except BluetoothctlError as e:
-            print(e)
-            return None
-
-        paired_devices = list(filter(None, (
-            self.parse_device_info(line)
-            for line in out
-        )))
-
-        return paired_devices
+        lines = self.get_output("paired-devices")
+        return self.parse_device_infos(lines)
 
     def get_discoverable_devices(self):
         """Filter paired devices out of available."""
@@ -123,21 +105,21 @@ class Bluetoothctl:
 
         return list(set(available) - set(paired))
 
+    RE_DEVICE_INFO = re.compile(r'\t(\w+): (.*)')
+
     def get_device_info(self, mac_address):
         """Get device info by mac address."""
-        try:
-            return  self.get_output("info " + mac_address)
-        except BluetoothctlError as e:
-            print(e)
-            return None
+        lines = self.get_output("info {}".format(mac_address))
+        return dict(
+            match.groups()
+            for match in map(self.RE_DEVICE_INFO.match, lines)
+            if match
+        )
 
     def pair(self, mac_address):
         """Try to pair with a device by mac address."""
-        try:
-            self.get_output("pair " + mac_address, 4)
-        except BluetoothctlError as e:
-            print(e)
-            return None
+        if not self.get_output("pair {}".format(mac_address), 4):
+            return False
 
         result = self.child.expect(["Failed to pair", "Pairing successful", pexpect.EOF])
         success = result == 1
@@ -145,11 +127,8 @@ class Bluetoothctl:
 
     def remove(self, mac_address):
         """Remove paired device by mac address, return success of the operation."""
-        try:
-            self.get_output("remove " + mac_address, 3)
-        except BluetoothctlError as e:
-            print(e)
-            return None
+        if not self.get_output("remove {}".format(mac_address), 3):
+            return False
 
         result = self.child.expect(["not available", "Device has been removed", pexpect.EOF])
         success = result == 1
@@ -157,11 +136,8 @@ class Bluetoothctl:
 
     def connect(self, mac_address):
         """Try to connect to a device by mac address."""
-        try:
-            self.get_output("connect " + mac_address, 2)
-        except BluetoothctlError as e:
-            print(e)
-            return None
+        if not self.get_output("connect {}".format(mac_address), 2):
+            return False
 
         result = self.child.expect(["Failed to connect", "Connection successful", pexpect.EOF])
         success = result == 1
@@ -169,10 +145,7 @@ class Bluetoothctl:
 
     def disconnect(self, mac_address):
         """Try to disconnect to a device by mac address."""
-        try:
-            self.get_output("disconnect " + mac_address, 2)
-        except BluetoothctlError as e:
-            print(e)
+        if not self.get_output("disconnect {}".format(mac_address), 2):
             return None
 
         result = self.child.expect(["Failed to disconnect", "Successful disconnected", pexpect.EOF])
