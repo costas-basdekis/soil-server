67ed7818a8c0c3f5347c130dd15c958c3f72bc24
Costas Basdekis <costas.basdekis@gmail.com>
2020-08-21 09:01:51 +0100

    More robust connections, update device names

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 import datetime
 import itertools
+import re
 import time
 
 import bluetooth as bt
 
+import bluetoothctl
 
-BLUETOOTH_NAME = 'HC-05'
+BLUETOOTH_PATTERN = re.compile(r'^SOIL-\d+$')
 SLEEP_TIME = datetime.timedelta(seconds=1)
 SEARCH_INTERVAL = datetime.timedelta(minutes=1)
+
+BT_ERROR_NO_SUCH_DEVICE = 19
+BT_ERROR_DISCONNECTED = 107
+BT_ERROR_HOST_IS_DOWN = 112
 
 
 def main():
@@ -61,14 +67,17 @@ def close_all_connections(socket_by_address, buffer_by_socket):
 def find_and_connect(socket_by_address, buffer_by_socket):
     print("Finding devices")
     mac_addresses_by_name = get_mac_addresses_by_name()
-    soil_addresses = mac_addresses_by_name.get(BLUETOOTH_NAME, [])
-    print("Got {} names, {} for {}".format(len(mac_addresses_by_name), len(soil_addresses), BLUETOOTH_NAME))
+    soil_addresses = sum((
+        addresses
+        for name, addresses in mac_addresses_by_name.items()
+        if BLUETOOTH_PATTERN.match(name)
+    ), [])
+    print("Got {} names, {} matching {}".format(
+        len(mac_addresses_by_name), len(soil_addresses), BLUETOOTH_PATTERN.pattern))
     for address in soil_addresses:
         socket_by_address.setdefault(address, None)
-    create_connections(socket_by_address)
+    create_connections(socket_by_address, buffer_by_socket)
     print("Connected to {} devices".format(len(socket_by_address.values())))
-    for socket in socket_by_address.values():
-        buffer_by_socket.setdefault(socket, b"")
 
 
 def is_socket_still_connected(socket):
@@ -76,7 +85,7 @@ def is_socket_still_connected(socket):
         socket.getpeername()
         return True
     except bt.BluetoothError as e:
-        if e.errno == 107:
+        if e.errno == BT_ERROR_DISCONNECTED:
             return False
         raise
 
@@ -91,21 +100,52 @@ def get_new_data(socket):
     return new_data
 
 
-def create_connections(socket_by_address):
-    for address, socket in socket_by_address.items():
+def create_connections(socket_by_address, buffer_by_socket):
+    for address, socket in list(socket_by_address.items()):
         if not socket:
             socket = socket_by_address[address] = bt.BluetoothSocket(bt.RFCOMM)
-            socket.connect((address, 1))
+            try:
+                socket.connect((address, 1))
+            except bt.BluetoothError as e:
+                del socket_by_address[address]
+                if e.errno == BT_ERROR_HOST_IS_DOWN:
+                    print("Could not connect to {}: host is down".format(address))
+                    continue
+                raise
             socket.setblocking(False)
+            buffer_by_socket.setdefault(socket, b"")
             print("Connected to {}".format(address))
 
 
 def get_mac_addresses_by_name():
+    return get_mac_addresses_by_name_with_bctl()
+
+
+def get_mac_addresses_by_name_with_bctl():
+    bctl = bluetoothctl.Bluetoothctl()
+    bctl.start_scan()
+    devices = bctl.get_paired_devices()
+    mac_addresses_by_name = {
+        name: [device['mac_address'] for device in grouped_devices]
+        for name, grouped_devices
+        in itertools.groupby(sorted(devices, key=lambda device: device['name']), key=lambda device: device['name'])
+    }
+    return mac_addresses_by_name
+
+
+def get_mac_addresses_by_name_with_bluez():
+    try:
+        devices = bt.discover_devices(duration=8, lookup_names=True, flush_cache=True)
+    except bt.BluetoothError as e:
+        if e.errno == BT_ERROR_NO_SUCH_DEVICE:
+            print("Warning: Bluetooth is turned off")
+            return {}
+        raise
     mac_addresses_by_name = {
         name: [address for _, address in names_and_addresses]
         for name, names_and_addresses in itertools.groupby(sorted(
             (name, address)
-            for address, name in bt.discover_devices(lookup_names=True)
+            for address, name in devices
         ), key=lambda name_and_address: name_and_address[0])
     }
     return mac_addresses_by_name
